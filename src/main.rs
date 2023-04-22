a9d67ee2cda6d7a5cbeb8728e09ad4c128d46388
GossiperLoturot <ts20030211@gmail.com>
2023-04-22 18:49:34 +0900

    refactor: wgpuの処理をカプセル化

--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,106 @@ fn main() {
     pollster::block_on(start());
 }
 
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: winit::window::Window,
+}
+
+impl State {
+    async fn new_async(window: winit::window::Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default(), None)
+            .await
+            .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            width: size.width,
+            height: size.height,
+            format: surface_capabilities.formats[0],
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    }
+
+    fn render(&mut self) {
+        let output = self.surface.get_current_texture().unwrap();
+
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+            label: None,
+        });
+
+        self.queue.submit([encoder.finish()]);
+
+        output.present();
+    }
+
+    fn match_id(&self, id: winit::window::WindowId) -> bool {
+        self.window.id() == id
+    }
+}
+
 async fn start() {
     let event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::WindowBuilder::new()
         .build(&event_loop)
         .unwrap();
 
-    let window_size = window.inner_size();
+    let mut state = State::new_async(window).await;
 
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
-
-    let surface = unsafe { instance.create_surface(&window) }.unwrap();
-
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            compatible_surface: Some(&surface),
-            force_fallback_adapter: false,
-        })
-        .await
-        .unwrap();
-
-    let (device, queue) = adapter
-        .request_device(&wgpu::DeviceDescriptor::default(), None)
-        .await
-        .unwrap();
-
-    let surface_capabilities = surface.get_capabilities(&adapter);
-
-    let config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        width: window_size.width,
-        height: window_size.height,
-        format: surface_capabilities.formats[0],
-        present_mode: surface_capabilities.present_modes[0],
-        alpha_mode: surface_capabilities.alpha_modes[0],
-        view_formats: vec![],
-    };
-
-    surface.configure(&device, &config);
-
+    use winit::event::Event;
     event_loop.run(move |event, _, _| match event {
-        winit::event::Event::RedrawRequested(window_id) if window_id == window.id() => {
-            let output = surface.get_current_texture().unwrap();
-
-            let view = output
-                .texture
-                .create_view(&wgpu::TextureViewDescriptor::default());
-
-            let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
-
-            encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-                label: None,
-            });
-
-            queue.submit([encoder.finish()]);
-
-            output.present();
-        }
+        Event::RedrawRequested(id) if state.match_id(id) => state.render(),
         _ => {}
     });
 }
