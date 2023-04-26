0c93e8f5442334bfdbd4823f3d50aeb90243c71e
GossiperLoturot <ts20030211@gmail.com>
2023-04-26 14:23:58 +0900

    tetris-wgpu refactor: 頂点バッファのレイアウトを頂点クラスに所持

--- tetris-wgpu/src/main.rs
+++ tetris-wgpu/src/main.rs
@@ -11,6 +11,19 @@ struct Vertex {
     color: [f32; 3],
 }
 
+impl Vertex {
+    const ATTRIBUTES: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+
+    fn layout<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBUTES,
+        }
+    }
+}
+
 const VERTICES: &[Vertex] = &[
     Vertex {
         position: [0.0, 0.5, 0.0],
@@ -83,22 +96,7 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
-        let vertex_buffer_layout = wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-            ],
-        };
+        let vertex_buffer_layout = Vertex::layout();
 
         let num_indices = INDICES.len() as u32;
 
