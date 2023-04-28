8ac8eecb8cd927d898d881724ae2de0b553aa8b4
GossiperLoturot <ts20030211@gmail.com>
2023-04-28 23:34:58 +0900

    tetris-wgpu change: ブロックの色の定義を変更

--- tetris-wgpu/src/render/constants.rs
+++ tetris-wgpu/src/render/constants.rs
@@ -5,11 +5,11 @@ pub const HEIGHT: f32 = 20.0;
 pub mod color {
     pub const BG_DEFAULT: [f32; 3] = [1.000, 1.000, 1.000];
     pub const BG_SUBTLE:  [f32; 3] = [0.921, 0.938, 0.955];
-    pub const FG_CYAN:    [f32; 3] = [0.000, 0.000, 0.000];
-    pub const FG_YELLOW:  [f32; 3] = [0.000, 0.000, 0.000];
-    pub const FG_GREEN:   [f32; 3] = [0.010, 0.212, 0.038];
-    pub const FG_RED:     [f32; 3] = [0.624, 0.015, 0.027];
-    pub const FG_BLUE:    [f32; 3] = [0.002, 0.141, 0.701];
-    pub const FG_ORANGE:  [f32; 3] = [0.502, 0.072, 0.000];
-    pub const FG_PURPLE:  [f32; 3] = [0.223, 0.080, 0.737];
+    pub const FG_CYAN:    [f32; 3] = [0.000, 0.666, 1.000];
+    pub const FG_YELLOW:  [f32; 3] = [1.000, 0.666, 0.000];
+    pub const FG_GREEN:   [f32; 3] = [0.133, 1.000, 0.000];
+    pub const FG_RED:     [f32; 3] = [1.000, 0.000, 0.133];
+    pub const FG_BLUE:    [f32; 3] = [0.000, 0.133, 1.000];
+    pub const FG_ORANGE:  [f32; 3] = [1.000, 0.133, 0.000];
+    pub const FG_PURPLE:  [f32; 3] = [0.133, 0.000, 1.000];
 }
